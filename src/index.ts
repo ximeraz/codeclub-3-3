d4d20474392ccd35d7f4efa96dfcddf0c51373a3
Leidy Ximena Salazar Barrera <leidy@cloudflare.com>
2024-04-23 22:43:48 -0600

    second commit

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,27 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+// src/index.js
+var src_default = {
+  async fetch(request, env, ctx) {
+    if(request.url == "https://www.cloudflareworkers.com/test") {
+           return new Response('Hello worker!', {
+               headers: {
+                   'content-type': 'text/plain',
+               },
+           });
+       }
+       else{
+           return new Response('Error Worker! Wrong URL', {
+               headers: {
+                   'content-type': 'text/plain',
+               },
+           });
+       }
+  }
 };
+export {
+  src_default as default
+};
+//# sourceMappingURL=index.js.map
+
